d2c9dcba8712cbe892d88d357277fb8dca7d44f4
DevKike <alvaronarvaez151@gmail.com>
2023-12-26 01:16:51 -0500

    Logic register was improved

--- src/modules/user/controller/user.controller.js
+++ src/modules/user/controller/user.controller.js
@@ -1,8 +1,17 @@
 const { create } = require("../service/user.service");
 const { hash } = require("../../../util/bcrypt");
+const User = require("../model/user.model").User;
 
 const registerUser = async (user) => {
   try {
+    const isEmailExist = await User.findOne({
+      where: {email: user.email}
+    });
+    
+    if(isEmailExist) {
+      throw new Error("Email already in use");
+    }
+
     const password = hash(user.password);
     const newUser = await create({ ...user, password });
     return newUser;
