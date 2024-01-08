c3809936f6e3715f8c3563805e60c4d01d94d037
DevKike <alvaronarvaez151@gmail.com>
2024-01-07 22:57:17 -0500

    ToDo Schema was added

--- src/modules/todo/todo.router.js
+++ src/modules/todo/todo.router.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const createToDo = require("./controller/todo.controller");
 const authToken = require("../../middleware/authToken.middleware");
+const schemaValidator = require("../../middleware/schemaValidator.middleware");
+const createToDoSchema = require("./schema/todo.schema");
 
 const toDoRouter = express.Router();
 
-toDoRouter.post("/create", authToken(), async (req, res) => {
+toDoRouter.post("/create", authToken(), schemaValidator(createToDoSchema), async (req, res) => {
   try {
-
     const toDoData = await createToDo(req.body, req.user); 
 
     res.status(201).json({
