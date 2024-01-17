58a304a866e6e152c5e430bca65f1332eef34b25
DevKike <alvaronarvaez151@gmail.com>
2024-01-17 00:34:33 -0500

    User service test was added

--- __test__/src/modules/user/service/user.service.test.js
+++ __test__/src/modules/user/service/user.service.test.js
@@ -1,4 +1,4 @@
-const { register, findUserBy } = require("../../../../../src/modules/user/service/user.service");
+const { register, findUserBy, update } = require("../../../../../src/modules/user/service/user.service");
 const { models } = require("../../../../../src/db/sequelize");
 
 jest.mock("../../../../../src/db/sequelize", () => ({
@@ -6,6 +6,8 @@ jest.mock("../../../../../src/db/sequelize", () => ({
     User: {
       create: jest.fn(),
       findByPk: jest.fn(),
+      findOne: jest.fn(),
+      update: jest.fn(),
     },
   },
 }));
@@ -55,5 +57,47 @@ describe("USER SERVICE TEST", () => {
     const result = await findUserBy({ id: userId });
 
     expect(result).toEqual(user);
+  });
+
+  it("should find user by other properties successfully", async () => {
+    const user = { id: 1, ...userData};
+
+    models.User.findOne.mockResolvedValueOnce(user);
+
+    const result = await findUserBy({ email: userData.email });
+
+    expect(result).toEqual(user);
+    expect(models.User.findOne).toHaveBeenCalledWith({ where: { email: userData.email } });
+  });
+
+  it("should throw an error if something fails", async () => {
+    const error = new Error("error");
+
+    models.User.findByPk.mockRejectedValueOnce(error);
+
+    await expect(findUserBy({ id: 1 })).rejects.toThrow(error);
+  });
+
+  it("should update data of a user successfully", async () => {
+    const updatedUser = { id: 1, ...userData, name: "Steve Doe" };
+    models.User.update.mockResolvedValueOnce([1]);
+
+    const result = await update(updatedUser);
+
+    expect(result).toEqual([1]);
+    expect(models.User.update).toHaveBeenCalledWith({ ...updatedUser }, { where: { id: updatedUser.id } });
+  });
+
+  it("should throw an error if update fails", async () => {
+    const mockError = new Error("User update failed");
+    models.User.update.mockRejectedValueOnce(mockError);
+
+    try {
+      await update(userData);
+      throw new Error('Expected an error to be thrown');
+    } catch (error) {
+      expect(error).toBe(mockError);
+    }
+    expect(models.User.update).toHaveBeenCalledWith({ ...userData }, { where: { id: userData.id } });
   });
 });
