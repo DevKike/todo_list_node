66798f51af46cec77243bcc41eaae52d1d85f47c
DevKike <alvaronarvaez151@gmail.com>
2024-01-27 20:12:30 -0500

    Worked on toDo schema testing

---
+++ __test__/src/modules/todo/schema/todo.schema.test.js
@@ -0,0 +1,70 @@
+const { invalid } = require("joi");
+const {
+  createToDoSchema,
+  updateToDoSchema,
+} = require("../../../../../src/modules/todo/schema/todo.schema");
+
+const validInput = {
+  title: "Test title",
+  description: "Test description",
+  finish: false,
+};
+
+describe("TODO SCHEMA TEST", () => {
+  it("should create schema with required title and description fields", () => {
+    const schema = createToDoSchema;
+    const result = schema.validate({
+      title: validInput.title,
+      description: validInput.description,
+    });
+
+    expect(result.error).toBeUndefined();
+  });
+
+  it("should create schema with optional finish field", () => {
+    const schema = createToDoSchema;
+    const result = schema.validate({
+      title: validInput.title,
+      description: validInput.description,
+      finish: true,
+    });
+
+    expect(result.error).toBeUndefined();
+  });
+
+  it("should fail to create schema when title is missing", () => {
+    const schema = createToDoSchema;
+    const result = schema.validate({ description: validInput.description });
+
+    expect(result.error).toBeDefined();
+  });
+
+  it("should fail to create schema when description is missing", () => {
+    const schema = createToDoSchema;
+    const result = schema.validate({ title: validInput.title });
+
+    expect(result.error).toBeDefined();
+  });
+
+  it("should fail to create schema when title is less than 3 characters", () => {
+    const schema = createToDoSchema;
+    const result = schema.validate({
+      title: "Te",
+      description: validInput.description,
+    });
+
+    expect(result.error).toBeDefined();
+  });
+
+  it("should validate a valid updateToDo object", () => {
+    const partialUpdateInput = {
+      title: "UpdatedTitle",
+      description: "UpdatedDescription",
+      finish: false,
+    };
+
+    const validationResult = updateToDoSchema.validate(partialUpdateInput);
+
+    expect(validationResult.error).toBeUndefined();
+  });
+});
